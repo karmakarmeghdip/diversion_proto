6fb45fbfc0428b48662f46f8f61457db20617e16
karmakarmeghdip <karmakarmeghdip@gmail.com>
2025-02-08 13:36:20 +0530

    first commit

---
+++ server.ts
@@ -0,0 +1,26 @@
+import index from './index.html';
+
+Bun.serve({
+  static: {
+    '/': index
+  },
+  development: true,
+  async fetch(request, server) {
+    if (request.url.endsWith('/session')) {
+      const r = await fetch("https://api.openai.com/v1/realtime/sessions", {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          model: "gpt-4o-realtime-preview-2024-12-17",
+          voice: "verse",
+        }),
+      });
+      const data = await r.json();
+      return new Response(JSON.stringify(data));
+    }
+    return new Response("Not Found", { status: 404 })
+  },
+})
