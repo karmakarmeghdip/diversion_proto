d046fcda9fb3be6a0e4afa135b420305dc8266ae
karmakarmeghdip <karmakarmeghdip@gmail.com>
2025-02-23 08:10:02 +0530

    feat: update authentication flow and enhance UI interactions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
-import { init, pause, play } from './ai'
+import { init, pause, play, done } from './ai'
 import { draw } from './visualisation';
 
 
 init();
 draw();
 
+let isPaused = false;
+
 // Handler Assignments
-document.getElementById("pause")?.addEventListener("click", pause);
-document.getElementById("play")?.addEventListener("click", play);
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM loaded");
+  document.getElementById("mic-btn")?.addEventListener("click", () => {
+    console.log("Mic button clicked");
+    if (isPaused) {
+      console.log("Resuming audio transmission");
+      play();
+      isPaused = false;
+    } else {
+      console.log("Pausing audio transmission");
+      pause();
+      isPaused = true;
+    }
+  });
+  document.getElementById("done-btn")?.addEventListener("click", () => {
+    console.log("Done button clicked");
+    done();
+  });
+});
